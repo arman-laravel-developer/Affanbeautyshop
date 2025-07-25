4ab2869cd644b9fffa3cde3f6c0e95a0a6bb5986
Md Arman Ali <mdarmanali070@gmail.com>
2025-07-26 00:55:35 +0600

    Update order-confirmation.blade.php

--- resources/views/front/pages/order-confirmation.blade.php
+++ resources/views/front/pages/order-confirmation.blade.php
@@ -24,7 +24,21 @@
         .order-summary {
             margin-top: 30px;
         }
+        .contact-us-box {
+            background-color: #ffffff;
+            border: 1px solid #dee2e6;
+            padding: 30px;
+            border-radius: 8px;
+            margin-top: 40px;
+        }
+        .contact-us-box i {
+            margin-right: 8px;
+        }
+        .contact-us-box a:hover {
+            text-decoration: none;
+        }
     </style>
+
     <!-- Thank You Page Content -->
     <div class="thank-you-page">
         <div class="container">
@@ -42,6 +56,7 @@
                     </div>
                 </div>
             </div>
+
             <!-- Order Summary and Details -->
             <div class="row justify-content-center order-summary">
                 <div class="col-md-8">
@@ -51,148 +66,160 @@
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
-                                <table class="table nowrap w-100 ">
+                                <table class="table nowrap w-100">
                                     <thead>
-                                    <tr>
-                                        <th>Item</th>
-                                        <th class="text-center">Variant</th>
-                                        <th class="text-center">Price</th>
-                                        <th class="text-center">Quantity</th>
-                                        <th class="text-center">Total</th>
-                                    </tr>
+                                        <tr>
+                                            <th>Item</th>
+                                            <th class="text-center">Variant</th>
+                                            <th class="text-center">Price</th>
+                                            <th class="text-center">Quantity</th>
+                                            <th class="text-center">Total</th>
+                                        </tr>
                                     </thead>
                                     <tbody>
-                                    @php
-                                        $withOutDiscount = 0;
-                                        $discount = 0;
-                                    @endphp
-                                    @foreach($order->orderDetails as $orderDetail)
                                         @php
-                                            $sizeName = $orderDetail->size->name ?? null;
-                                            $colorName = $orderDetail->color->name ?? null;
+                                            $withOutDiscount = 0;
+                                            $discount = 0;
                                         @endphp
-                                        @php
-                                            $product = \App\Models\Product::find($orderDetail->product_id);
-                                        if ($orderDetail->discount_type == 2) {
-                                            $newDiscount = ($orderDetail->price * ($orderDetail->discount / 100));
-                                        } else {
-                                            $newDiscount = $orderDetail->discount;
-                                        }
-                                        $totalAmount = $orderDetail->price+$newDiscount;
-                                        $withOutDiscount += $totalAmount * $orderDetail->qty;
-                                        $discount += $newDiscount * $orderDetail->qty;
-                                        @endphp
-                                        <tr>
-                                            <td style="padding-top: 2%; padding-bottom: 2%">
-                                                <a href="{{route('product.show', ['id' => $orderDetail->product->id, 'slug' => $orderDetail->product->slug])}}">{{\Illuminate\Support\Str::limit($orderDetail->product->name, 30)}}</a>
-                                            </td>
-                                            <td class="text-center" style="padding-top: 2%; padding-bottom: 2%">@if($sizeName || $colorName)
-                                                    @if($sizeName) {{ $sizeName }} @endif
-                                                    @if($sizeName && $colorName) / @endif
-                                                    @if($colorName) {{ $colorName }} @endif
-                                                @else
-                                                    N/A
-                                                @endif</td>
-                                            <td class="text-center" style="padding-top: 2%; padding-bottom: 2%">
-                                                @if($orderDetail->discount > 0)
-                                                    <del class="text-danger">&#2547; {{number_format($orderDetail->price+$newDiscount, 2)}}</del>
-                                                    <br>
-                                                @endif
-                                                &#2547; {{number_format($orderDetail->price, 2)}}
-                                            </td>
-                                            <td class="text-center" style="padding-top: 2%; padding-bottom: 2%">{{$orderDetail->qty}}</td>
-                                            <td class="text-center" style="padding-top: 2%; padding-bottom: 2%">
-                                                @if($orderDetail->discount > 0)
-                                                    <del class="text-danger">&#2547; {{number_format($totalAmount * $orderDetail->qty, 2)}}</del>
-                                                    <br>
-                                                @endif
-                                                &#2547; {{number_format($orderDetail->price * $orderDetail->qty, 2)}}
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                    <!-- Add more items as needed -->
+                                        @foreach($order->orderDetails as $orderDetail)
+                                            @php
+                                                $sizeName = $orderDetail->size->name ?? null;
+                                                $colorName = $orderDetail->color->name ?? null;
+                                                $product = \App\Models\Product::find($orderDetail->product_id);
+                                                $newDiscount = $orderDetail->discount_type == 2 ? ($orderDetail->price * ($orderDetail->discount / 100)) : $orderDetail->discount;
+                                                $totalAmount = $orderDetail->price + $newDiscount;
+                                                $withOutDiscount += $totalAmount * $orderDetail->qty;
+                                                $discount += $newDiscount * $orderDetail->qty;
+                                            @endphp
+                                            <tr>
+                                                <td style="padding-top: 2%; padding-bottom: 2%">
+                                                    <a href="{{route('product.show', ['id' => $orderDetail->product->id, 'slug' => $orderDetail->product->slug])}}">
+                                                        {{ \Illuminate\Support\Str::limit($orderDetail->product->name, 30) }}
+                                                    </a>
+                                                </td>
+                                                <td class="text-center" style="padding-top: 2%; padding-bottom: 2%">
+                                                    @if($sizeName || $colorName)
+                                                        @if($sizeName) {{ $sizeName }} @endif
+                                                        @if($sizeName && $colorName) / @endif
+                                                        @if($colorName) {{ $colorName }} @endif
+                                                    @else
+                                                        N/A
+                                                    @endif
+                                                </td>
+                                                <td class="text-center" style="padding-top: 2%; padding-bottom: 2%">
+                                                    @if($orderDetail->discount > 0)
+                                                        <del class="text-danger">&#2547; {{ number_format($orderDetail->price + $newDiscount, 2) }}</del><br>
+                                                    @endif
+                                                    &#2547; {{ number_format($orderDetail->price, 2) }}
+                                                </td>
+                                                <td class="text-center" style="padding-top: 2%; padding-bottom: 2%">{{ $orderDetail->qty }}</td>
+                                                <td class="text-center" style="padding-top: 2%; padding-bottom: 2%">
+                                                    @if($orderDetail->discount > 0)
+                                                        <del class="text-danger">&#2547; {{ number_format($totalAmount * $orderDetail->qty, 2) }}</del><br>
+                                                    @endif
+                                                    &#2547; {{ number_format($orderDetail->price * $orderDetail->qty, 2) }}
+                                                </td>
+                                            </tr>
+                                        @endforeach
                                     </tbody>
                                     <tfoot>
-                                    @php
-                                        $totalFinal = $order->grand_total + $order->shipping_cost;
-                                    @endphp
-                                    <tr>
-                                        <th colspan="4" class="text-right"><b>Subtotal:</b></th>
-                                        <th class="text-center">&#2547; {{number_format($withOutDiscount, 2)}}</th>
-                                    </tr>
-                                    <tr>
-                                        <th colspan="4" class="text-right"><b>Shipping cost:</b></th>
-                                        @if($order->shipping_cost > 0)
-                                        <th class="text-center">&#2547; {{number_format($order->shipping_cost, 2)}}</th>
-                                        @else
-                                            <th class="text-center">Shipping is free</th>
-                                        @endif
-                                    </tr>
-                                    <tr>
-                                        <th colspan="4" class="text-right"><b>Discount:</b></th>
-                                        <th class="text-center">&#2547; {{number_format($discount, 2)}}</th>
-                                    </tr>
-                                    <tr>
-                                        <th colspan="4" class="text-right"><b>Grand Total:</b></th>
-                                        <th class="text-center">&#2547; {{number_format($totalFinal, 2)}}</th>
-                                    </tr>
+                                        @php
+                                            $totalFinal = $order->grand_total + $order->shipping_cost;
+                                        @endphp
+                                        <tr>
+                                            <th colspan="4" class="text-right"><b>Subtotal:</b></th>
+                                            <th class="text-center">&#2547; {{ number_format($withOutDiscount, 2) }}</th>
+                                        </tr>
+                                        <tr>
+                                            <th colspan="4" class="text-right"><b>Shipping cost:</b></th>
+                                            @if($order->shipping_cost > 0)
+                                                <th class="text-center">&#2547; {{ number_format($order->shipping_cost, 2) }}</th>
+                                            @else
+                                                <th class="text-center">Shipping is free</th>
+                                            @endif
+                                        </tr>
+                                        <tr>
+                                            <th colspan="4" class="text-right"><b>Discount:</b></th>
+                                            <th class="text-center">&#2547; {{ number_format($discount, 2) }}</th>
+                                        </tr>
+                                        <tr>
+                                            <th colspan="4" class="text-right"><b>Grand Total:</b></th>
+                                            <th class="text-center">&#2547; {{ number_format($totalFinal, 2) }}</th>
+                                        </tr>
                                     </tfoot>
                                 </table>
                             </div>
                         </div>
                     </div>
+
                     <div class="text-center mt-4">
                         @if(Session::get('customer_id'))
-                            <a href="{{route('customer.dashboard')}}" class="btn btn-primary">Go to Dashboard</a>
+                            <a href="{{ route('customer.dashboard') }}" class="btn btn-primary">Go to Dashboard</a>
                         @else
-                            <a href="{{route('home')}}" class="btn btn-primary">Continue Shopping</a>
+                            <a href="{{ route('home') }}" class="btn btn-primary">Continue Shopping</a>
                         @endif
-                        <a href="#" class="btn btn-info" onclick="event.preventDefault(); document.getElementById('invoiceDownload').submit();"><i class="fas fa-download"></i> Download Invoice</a>
-                        <form action="{{route('invoice.download', ['id' => $order->id])}}" method="POST" id="invoiceDownload">
+                        <a href="#" class="btn btn-info" onclick="event.preventDefault(); document.getElementById('invoiceDownload').submit();">
+                            <i class="fas fa-download"></i> Download Invoice
+                        </a>
+                        <form action="{{ route('invoice.download', ['id' => $order->id]) }}" method="POST" id="invoiceDownload">
                             @csrf
                         </form>
                     </div>
+
+                    <!-- Contact Info Section -->
+                    <div class="contact-us-box text-center">
+                        <h5 class="text-success font-weight-bold mb-3">Need Help or Have Questions?</h5>
+                        <p class="mb-3">We're here to help you. Connect with us anytime for assistance.</p>
+                        <div class="d-flex justify-content-center align-items-center gap-3 flex-wrap">
+                            <a href="{{ $generalSetting->facebook_url }}" target="_blank" class="btn btn-outline-primary px-4">
+                                <i class="fab fa-facebook-f"></i> Visit Our Facebook Page
+                            </a>
+                            <a href="tel:+88{{ $generalSetting->mobile }}" class="btn btn-outline-success px-4">
+                                <i class="fas fa-phone"></i> Call Us: {{ $generalSetting->mobile }}
+                            </a>
+                        </div>
+                    </div>
+                    <!-- End Contact Info -->
                 </div>
             </div>
         </div>
     </div>
 
     <script>
-       dataLayer.push({ ecommerce: null });
-            dataLayer.push({
-                event : "purchase",
-                ecommerce: {
-                    currency: "BDT",
-                    transaction_id: "{{ $order->order_code }}",
-                    affiliation : "",
-                    value : "{{ $order->grand_total }}",
-                    tax : "",
-                    shipping : "{{ $order->shipping_cost }}",
-                    coupon : "",
-                    items : [
-                        @foreach ($order->orderDetails as $orderDetail)
-                        {
-                            item_name : "{{ $orderDetail->product->name }}",
-                            item_id : "{{ $orderDetail->product_id }}",
-                            price : "{{ $orderDetail->price }}",
-                            item_brand : "{{ $orderDetail->product->brand->name }}",
-                            item_category: "{{ $orderDetail->product->category->category_name ?? '' }}",
-                            item_variant : "{{ $orderDetail->size_id ? $orderDetail->size->name : '' }}{{ $orderDetail->size_id && $orderDetail->color_id ? ' / ' : '' }}{{ $orderDetail->color_id ? $orderDetail->color->name : '' }}",
-                            quantity : {{ $orderDetail->qty }}
-                        },
-                        @endforeach
-                    ]
-                },
-                customer: {
-                    shipping: {
-                        name: "{{ $order->name }}",
-                        email: "{{ $order->email }}",
-                        mobile: "{{ $order->mobile }}",
-                        address: "{{ $order->address }}",
-                        alt_phone: "{{ $order->alt_mobile }}"
-                    }
+        dataLayer.push({ ecommerce: null });
+        dataLayer.push({
+            event : "purchase",
+            ecommerce: {
+                currency: "BDT",
+                transaction_id: "{{ $order->order_code }}",
+                affiliation : "",
+                value : "{{ $order->grand_total }}",
+                tax : "",
+                shipping : "{{ $order->shipping_cost }}",
+                coupon : "",
+                items : [
+                    @foreach ($order->orderDetails as $orderDetail)
+                    {
+                        item_name : "{{ $orderDetail->product->name }}",
+                        item_id : "{{ $orderDetail->product_id }}",
+                        price : "{{ $orderDetail->price }}",
+                        item_brand : "{{ $orderDetail->product->brand->name }}",
+                        item_category: "{{ $orderDetail->product->category->category_name ?? '' }}",
+                        item_variant : "{{ $orderDetail->size_id ? $orderDetail->size->name : '' }}{{ $orderDetail->size_id && $orderDetail->color_id ? ' / ' : '' }}{{ $orderDetail->color_id ? $orderDetail->color->name : '' }}",
+                        quantity : {{ $orderDetail->qty }}
+                    },
+                    @endforeach
+                ]
+            },
+            customer: {
+                shipping: {
+                    name: "{{ $order->name }}",
+                    email: "{{ $order->email }}",
+                    mobile: "{{ $order->mobile }}",
+                    address: "{{ $order->address }}",
+                    alt_phone: "{{ $order->alt_mobile }}"
                 }
-            });
+            }
+        });
     </script>
-
 @endsection
